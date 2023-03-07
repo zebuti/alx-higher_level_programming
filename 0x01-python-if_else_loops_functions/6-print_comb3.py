782c0d6ad8faf88db65c67ed37ebeedd8504a719
zebuti <102354890+zebuti@users.noreply.github.com>
2023-03-07 14:35:45 +0300

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-charend = ", "
-for i in range(0, 100):
-    digit2 = i % 10
-    digit1 = i / 10
-    if i < 10 and digit1 < digit2:
-        print("{}{}".format(0, i), end=charend)
-    else digit1 < digit2:
-        if i == 89:
-            charend = "\n"
-        print(i, end=charend)
+for i in range(0, 10):
+for j in range(1, 10):
+  if i >= j:
+     continue
+  elseif i == 8 and j == 9:
+      print("{}{}".format(i, j))
+  else:
+      print("{}{}, ".format(i, j), end="")
