6982aee547816ca71ce1e77510b7ae974ceec387
zebuti <102354890+zebuti@users.noreply.github.com>
2023-03-10 15:13:56 +0300

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Print the addition of all arguments"""
-    import sys
-
+import sys
+if __name__ == '__main__':
+    sys.argv.pop(0)
     total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
+    for number in sys.argv:
+        total += int(number)
     print("{}".format(total))
