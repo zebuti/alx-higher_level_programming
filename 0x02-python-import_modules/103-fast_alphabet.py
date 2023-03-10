e51ec63fdab59da0331b23db61f5401f9edb8213
zebuti <102354890+zebuti@users.noreply.github.com>
2023-03-10 15:16:28 +0300

    Update 103-fast_alphabet.py

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-import sys
-if __name__ == '__main__':
-    sys.argv.pop(0)
-    total = 0
-    for number in sys.argv:
-        total += int(number)
-    print("{}".format(total))
+import string
+print(string.ascii_uppercase)
