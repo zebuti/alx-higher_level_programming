06910596ee11935f80aa59b700d5c0ae7f0f4726
zebuti <102354890+zebuti@users.noreply.github.com>
2023-03-13 10:45:30 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    import sys
+import sys
 
-    sys.argv.pop(0)
-    argvlength = len(sys.argv)
+if __name__ != "__main__":
+    exit()
 
-    if (argvlength == 0):
-        print("0 arguments.")
-    elseif (argvlength == 1):
-        print("1 argument:")
-        print("{:d}: {}".format(len(sys.argv), sys.argv[0]))
-    else:
-        print("{:d} arguments:".format(argvlength))
-        number = 1
-        for argument in sys.argv:
-                print("{:d}: {}".format(number, argument))
-                number += 1
+argStr = "{:d} argument"
+argc = len(sys.argv) - 1
+if argc == 0:
+    argStr += 's.'
+elif argc == 1:
+    argStr += ':'
+else:
+    argStr += 's:'
+print(argStr.format(argc))
+
+i = 0
+for arg in sys.argv:
+    if i != 0:
+        print("{:d}: {:s}".format(i, arg))
+    i += 1
