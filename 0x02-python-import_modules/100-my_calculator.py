07be045210747899982fb23b1e32b5457f819d58
zebuti <102354890+zebuti@users.noreply.github.com>
2023-03-15 14:14:11 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-from sys import argv, exit
-from calculator_1 import add, sub, mul, div
-
-if __name__ == '__main__':
-    argv.pop(0)
-    argvlength = len(argv)
-
-    if (argvlength == 3):
-        a = int(argv[0])
-        b = int(argv[2])
-
-        if (argv[1] == "+"):
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif (argv[1] == "-"):
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif (argv[1] == "*"):
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif (argv[1] == "/"):
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-    else:
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    operators = ["+", "-", "*", "/"]
+    if argv_count is not 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    elif sys.argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] is "+":
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif sys.argv[2] is "-":
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif sys.argv[2] is "*":
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        elif sys.argv[2] is "/":
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
