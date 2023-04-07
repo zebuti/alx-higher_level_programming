09c1565e9be0586e4dcb8cb85b15fe6950d84113
zebuti <102354890+zebuti@users.noreply.github.com>
2023-04-07 16:57:27 +0300

    Update 4-main.py

--- 0x07-python-test_driven_development/4-main.py
+++ 0x07-python-test_driven_development/4-main.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-print_square = __import__('4-print_square').print_square
+"""Defines a square-printing function."""
 
-print_square(4)
-print("")
-print_square(10)
-print("")
-print_square(0)
-print("")
-print_square(1)
-print("")
-try:
-    print_square(-1)
-except Exception as e:
-    print(e)
-print("")
+
+def print_square(size):
+    """Print a square with the # character.
+    Args:
+        size (int): The height/width of the square.
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
