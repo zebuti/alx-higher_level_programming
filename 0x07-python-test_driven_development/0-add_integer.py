938d63fb442890e14f28159adc02988b8c26da66
zebuti <102354890+zebuti@users.noreply.github.com>
2023-04-07 16:40:44 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
+"""Module for add_integer method"""
+
+
 def add_integer(a, b=98):
-    if not isinstance(a, int) and not isinstance(a, float):
+    """Adds two integers.
+    Args:
+        a: first int.
+        b: second int, default value is 98.
+    Raises:
+        TypeError: if a, b are neither int nor float.
+    Returns:
+        sum of a and b.
+    """
+
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    elif type(b) not in (int, float):
         raise TypeError("b must be an integer")
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    return (a + b)
+    else:
+        return int(a) + int(b)
+
+    if __name__ == "__main__":
+        import doctest
+        doctest.testfile("tests/0-add_integer.txt")
