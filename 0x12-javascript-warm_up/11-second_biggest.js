7ec1534132b58c824663e3234e288aac6fe3e034
zebuti <102354890+zebuti@users.noreply.github.com>
2023-06-13 11:16:21 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,20 @@
 #!/usr/bin/node
-// searches the second biggest integer in the list of arguments.
 
-if (process.argv.length <= 3) {
-  console.log(0);
-} else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+function second (myArray) {
+  if (myArray.length === 2 || myArray.length === 3) { return (0); }
+
+  let max = myArray[2];
+  let secondMax = myArray[3];
+
+  for (let i = 2; i < myArray.length; i++) {
+    if (myArray[i] > max) {
+      secondMax = max;
+      max = myArray[i];
+    } else if (myArray[i] > secondMax && myArray[i] < max) {
+      secondMax = myArray[i];
+    }
+  }
+  return (secondMax);
 }
+
+console.log(second(process.argv));
