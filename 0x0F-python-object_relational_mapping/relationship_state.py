13a50b506ce3d3b4d56438c9a65b33d1b486d688
zebuti <102354890+zebuti@users.noreply.github.com>
2023-06-16 15:32:54 +0300

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""
-Contains State class and Base, an instance of declarative_base()
-"""
-from sqlalchemy import Column, Integer, String, MetaData
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, text
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+"""
+    Module that performs creates a States class based off of Base.
+"""
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+Base = declarative_base()
 
 
 class State(Base):
     """
-    Class with id and name attributes of each state
+        The ``States`` class which inherits from ``Base`` class.
     """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="states")
+
+    cities = relationship("City", backref="state", cascade="all, delete")
