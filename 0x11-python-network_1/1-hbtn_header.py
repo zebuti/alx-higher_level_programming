c44c64cb1b276221441d94c5bd3021f57608e732
zebuti <102354890+zebuti@users.noreply.github.com>
2023-07-03 16:18:45 +0300

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL.
-Usage: ./1-hbtn_header.py <URL>
+"""displays the value of the X-Request-Id variable found in
+the header of the response.
 """
-import sys
-import urllib.request
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    import urllib.request
+    import sys
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        head = response.headers.get('X-Request-Id')
+        print(head)
