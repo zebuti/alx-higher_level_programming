21567302a5ab226c2cc2e5d77c1d093b1b4cc6dc
zebuti <102354890+zebuti@users.noreply.github.com>
2023-07-25 14:16:59 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-// A script that reads and prints the content of a file
 
-const file = process.argv[2];
 const fs = require('fs');
+const filename = process.argv[2];
 
-fs.readFile(file, 'utf8', (error, data) => {
+fs.readFile(filename, 'utf-8', (error, content) => {
   if (error) {
     console.log(error);
   } else {
-    console.log(data);
+    console.log(content);
   }
 });
