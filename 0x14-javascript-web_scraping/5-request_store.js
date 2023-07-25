3c9639bdd138912568e4e59326b98ca722bc2f21
zebuti <102354890+zebuti@users.noreply.github.com>
2023-07-25 14:28:32 +0300

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,23 +1,18 @@
 #!/usr/bin/node
 
 const request = require('request');
+const fs = require('fs');
 const url = process.argv[2];
+const file = process.argv[3];
 
-request.get(url, { json: true }, (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) {
     console.log(error);
-    return;
-  }
-
-  const tasksCompleted = {};
-  body.forEach((todo) => {
-    if (todo.completed) {
-      if (!tasksCompleted[todo.userId]) {
-        tasksCompleted[todo.userId] = 1;
-      } else {
-        tasksCompleted[todo.userId] += 1;
+  } else {
+    fs.writeFile(file, body, 'utf8', (error) => {
+      if (error) {
+        console.log(error);
       }
-    }
-  });
-  console.log(tasksCompleted);
+    });
+  }
 });
